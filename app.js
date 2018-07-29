d6d296c71099e1bf3a970ab5d234836105d12724
LHansson <lhanssonatx@gmail.com>
2018-07-28 19:11:04 -0500

    coffee shark work

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ app.use(function(err, req, res, next) {
 });
 
 // listen on port 3000
-app.listen(3001, function () {
+app.listen(3000, function () {
   console.log('Express app listening on port 3000');
 });
